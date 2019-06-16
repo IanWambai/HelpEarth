47af3da6b85b9eb3ef8e1acfe51a86814f735135
Ian Wambai <3706215+IanWambai@users.noreply.github.com>
2019-06-16 18:34:54 +0300

    Oil & Gas/Agriculture
    Add prompts for oil & gas and agriculture industry actions and the actions to take
    
    Signed-off-by: Ian Wambai <3706215+IanWambai@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:quiver/async.dart';
-import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -18,8 +17,12 @@ class CountdownTimerPage extends StatefulWidget {
 class CountdownTimerPageState extends State<CountdownTimerPage> {
   int timeWeHaveLeft = 0;
   int progressSinceReportWasReleased = 0;
-  int currentNumber = 0;
-  final stepInSeconds = 1;
+  
+  int currentTimeCount = 0;
+  int timeCountOilAndGas = 0;
+  int timeCountAgriculture = 0;
+
+  final stepInMilliseconds = 1;
 
   CountdownTimerPageState() {
     processCurrentTime();
@@ -32,22 +35,17 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
     DateTime now = DateTime.now();
 
     timeWeHaveLeft = deadline.difference(now).inSeconds;
-    int timeSinceReportWasReleased =
-        deadline.difference(reportReleaseDate).inSeconds;
-    progressSinceReportWasReleased =
-        100 - ((timeWeHaveLeft / timeSinceReportWasReleased) * 100).toInt();
+    int timeSinceReportWasReleased = deadline.difference(reportReleaseDate).inSeconds;
+    progressSinceReportWasReleased = 100 - ((timeWeHaveLeft / timeSinceReportWasReleased) * 100).toInt();
   }
 
   setupCountdownTimer() {
-    CountdownTimer countDownTimer = new CountdownTimer(
-        new Duration(seconds: timeWeHaveLeft),
-        new Duration(seconds: stepInSeconds));
+    CountdownTimer countDownTimer = new CountdownTimer(new Duration(seconds: timeWeHaveLeft), new Duration(milliseconds: stepInMilliseconds));
 
     var sub = countDownTimer.listen(null);
     sub.onData((duration) {
-      currentNumber += stepInSeconds;
-
-      this.onTimerTick(currentNumber);
+      currentTimeCount += stepInMilliseconds;
+      this.onTimerTick(currentTimeCount);
     });
 
     sub.onDone(() {
@@ -58,14 +56,16 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
   void onTimerTick(int currentNumber) {
     setState(() {
       currentNumber = currentNumber;
+      timeCountOilAndGas += 1;
+      timeCountAgriculture += 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     final formatter = new NumberFormat("###,###,###,###,###,###");
-    int number = timeWeHaveLeft - currentNumber;
-    number += stepInSeconds;
+    int number = timeWeHaveLeft - currentTimeCount;
+    number += stepInMilliseconds;
 
     return new Scaffold(
         body: Column(
@@ -84,7 +84,8 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
           padding: const EdgeInsets.only(top: 18.0, bottom: 18.0),
           child: Text(
             formatter.format(number),
-            style: new TextStyle(color: Colors.black, fontSize: 48.0),
+            style: new TextStyle(
+                color: Colors.red, fontSize: 48.0, fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
         ),
@@ -95,7 +96,7 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
         ),
         Padding(
           padding: const EdgeInsets.only(
-              top: 64.0, bottom: 72.0, left: 64.0, right: 64.0),
+              top: 64.0, bottom: 64.0, left: 64.0, right: 64.0),
           child: Text(
             'If time runs out, life permanently gets worse. We\'re already out of time.',
             style: new TextStyle(color: Colors.black, fontSize: 18.0),
@@ -103,7 +104,83 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.only(left: 12.0, right: 12.0, bottom: 64.0),
+          padding: const EdgeInsets.only(bottom: 16),
+          child: Center(
+            child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                child: Container(
+                  width: 256,
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4),
+                          child: new Text(
+                            'Oil & Gas',
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 24.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        new Text(
+                          timeCountOilAndGas.toString() + ' tons of Carbon so far',
+                          style: new TextStyle(
+                              color: Colors.white, fontSize: 14.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  _doSomething(context);
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(12.0))),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: Center(
+            child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.red,
+                child: Container(
+                  width: 256,
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4),
+                          child: new Text(
+                            'Agriculture',
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 24.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        new Text(
+                          timeCountAgriculture.toString() + ' tons of Carbon so far',
+                          style: new TextStyle(
+                              color: Colors.white, fontSize: 14.0),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  _doSomething(context);
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(12.0))),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 64.0),
           child: Center(
             child: GestureDetector(
               onTap: () {
@@ -111,29 +188,7 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
               },
               child: new Text(
                 'Read The Research',
-                style: new TextStyle(color: Colors.black, fontSize: 24.0),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: 200.0,
-          width: 200.0,
-          decoration: new BoxDecoration(
-            color: Colors.red,
-            shape: BoxShape.circle,
-          ),
-          child: Center(
-            child: GestureDetector(
-              onTap: () {
-                Share.share('We have ' +
-                    formatter.format(timeWeHaveLeft) +
-                    ' seconds left to #HelpEarth. If Time runs out, life as you know it will permanently change for the worse.\n\nWe Are Already Out Of Time.');
-              },
-              child: new Text(
-                'Do Something',
-                style: new TextStyle(color: Colors.white, fontSize: 24.0),
+                style: new TextStyle(color: Colors.black, fontSize: 20.0),
                 textAlign: TextAlign.center,
               ),
             ),
@@ -151,28 +206,12 @@ class CountdownTimerPageState extends State<CountdownTimerPage> {
     }
   }
 
-  _openAboutPage(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => SecondRoute()));
+  _doSomething(BuildContext context) {
+//    Navigator.of(context)
+//        .push(MaterialPageRoute(builder: (context) => DoSomethingPage()));
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("About Help Earth"),
-        backgroundColor: Colors.cyan,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 64, bottom: 32),
-        child: Text(
-          'If Time runs out, life as you know it will permanently change for the worse.',
-          style: new TextStyle(color: Colors.black, fontSize: 18.0),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
-}
+//class DoSomethingPage extends StatefulWidget {
+//
+//}
